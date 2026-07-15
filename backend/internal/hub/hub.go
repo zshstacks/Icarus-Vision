063cecdad81da9a87e76630fe2c76da2fe0bbba2
zshstacks <uns4d123@gmail.com>
2026-07-15 22:53:50 +0300

    ADDED: hub struct

--- backend/internal/hub/hub.go
+++ backend/internal/hub/hub.go
@@ -1,1 +1,23 @@
 package hub
+
+import (
+	"icarus-vision/internal/pb"
+	"icarus-vision/internal/ws"
+)
+
+type Hub struct {
+	clients    map[*ws.Client]struct{}
+	broadcast  chan *pb.AttackEvent
+	register   chan *ws.Client
+	unregister chan *ws.Client
+}
+
+func NewHub() *Hub {
+	hub := Hub{
+		clients:    make(map[*ws.Client]struct{}),
+		broadcast:  make(chan *pb.AttackEvent, 100),
+		register:   make(chan *ws.Client),
+		unregister: make(chan *ws.Client),
+	}
+	return &hub
+}
